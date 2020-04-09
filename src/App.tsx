832cd5470822e3f1b112621336c714306a624702
Toma Otani <otani.toma@gmail.com>
2020-04-09 14:13:36 -0700

    5-2. JSX Grammar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const logoOptions = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "こんにちはReact";
+    const targets = ["World", "Kanae", "Yukina"];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          {/* オブジェクトに格納されたタグ属性値をまとめて渡す */}
+          <img {...logoOptions} />
+
+          {/* if文に相当、前に置いた値がtrueであれば、次に続く値が評価される */}
+          {title && <p>{title}</p>}
+
+          {/* elseを使う場合は三項演算子を使う */}
+          {title ? 'foo' : 'bar'}
+
+          {/* コレクションの繰り返し処理 */}
+          {targets.map(target => (
+            <p>Hello, {target}!</p>
+          ))}
+
+          {/* 一定条件に一致するものだけを表示 */}
+          <ul>
+          {targets.filter(t => t.length >= 6).map(t => <li>{t}</li>)}
+          </ul>
+
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;

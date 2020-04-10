e7c2bd901e7318a0e43b655244c13746cc64b9bb
Toma Otani <otani.toma@gmail.com>
2020-04-09 22:26:29 -0700

    7-2. props

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CharacterList, { Character } from './CharacterList';
+
 class App extends Component {
   render() {
     const logoOptions = {
@@ -12,28 +14,52 @@ class App extends Component {
     const title = "こんにちはReact";
     const targets = ["World", "Kanae", "Yukina"];
 
+    
+    // キャラクター一覧
+    const characters: Character[] = [
+      {
+        id: 1,
+        name: '羽咲 綾乃',
+        age: 16,
+        height: 151,
+      },
+      {
+        id: 2,
+        name: '荒垣 なぎさ',
+        age: 18,
+        height: 174,
+      },
+      {
+        id: 3,
+        name: '泉 理子',
+        age: 18,
+      },
+    ]
+
     return (
       <div className="App">
-        <header className="App-header">
+        
+        {/* オブジェクトに格納されたタグ属性値をまとめて渡す */}
+        <img {...logoOptions} alt="test"/>
 
-          {/* オブジェクトに格納されたタグ属性値をまとめて渡す */}
-          <img {...logoOptions} alt="test"/>
+        {/* if文に相当、前に置いた値がtrueであれば、次に続く値が評価される */}
+        {title && <p>{title}</p>}
 
-          {/* if文に相当、前に置いた値がtrueであれば、次に続く値が評価される */}
-          {title && <p>{title}</p>}
+        {/* elseを使う場合は三項演算子を使う */}
+        {title ? 'foo' : 'bar'}
 
-          {/* elseを使う場合は三項演算子を使う */}
-          {title ? 'foo' : 'bar'}
+        {/* コレクションの繰り返し処理 */}
+        {targets.map(target => (
+          <p>Hello, {target}!</p>
+        ))}
 
-          {/* コレクションの繰り返し処理 */}
-          {targets.map(target => (
-            <p>Hello, {target}!</p>
-          ))}
+        {/* 一定条件に一致するものだけを表示 */}
+        {targets.filter(t => t.length >= 6).map(t => <>{t}</>)}
 
-          {/* 一定条件に一致するものだけを表示 */}
-          {targets.filter(t => t.length >= 6).map(t => <>{t}</>)}
 
-        </header>
+        {/* オブジェクトをpropsとして子コンポーネントに渡す */}
+        <CharacterList school="北小町高校" characters={characters} />
+
       </div>
     )
   }

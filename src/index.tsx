a48296a3e4300acb430b738b9550eaddd7c3bf30
Toma Otani <otani.toma@gmail.com>
2020-04-09 23:07:50 -0700

    7-3 local state

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import State from './State';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <State />
   </React.StrictMode>,
   document.getElementById('root')
 );
